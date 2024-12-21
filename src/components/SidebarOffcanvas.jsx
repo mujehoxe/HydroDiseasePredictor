c96757242159b7129d2fa12d8271b9a78f7719dc
SarahRayaneRoufiaBenzai <77399392+SarahRayaneRoufiaBenzai@users.noreply.github.com>
2024-12-21 20:23:05 +0000

    sidebar fetch user farms

--- src/components/SidebarOffcanvas.jsx
+++ src/components/SidebarOffcanvas.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLanguage } from '../LanguageContext';
 import Dropdown from 'react-bootstrap/Dropdown';
@@ -9,6 +10,42 @@ import plusicone from '../icone/add-1--expand-cross-buttons-button-more-remove-p
 function Sidebar() {
   const { language, toggleLanguage } = useLanguage();
   const navigate = useNavigate();
+  const [farms, setFarms] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    // Fetch the user's farms from the API
+    const fetchFarms = async () => {
+      const token = localStorage.getItem('authToken'); // Retrieve the auth token
+      setLoading(true);
+      setError('');
+
+      try {
+        const response = await fetch(`https://vite-project-9cea.onrender.com/api/v1/users/1/farms`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${token}`, // Add the auth token here
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setFarms(data);
+        } else {
+          console.error('Failed to fetch farms:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching farms:', error);
+      }finally{
+        setLoading(false);
+      }
+    };
+
+    fetchFarms();
+  }, [1]);
+
   const handleAjout = () => {
     // Add your authentication logic here
     navigate('/Ajoutferme'); // Redirect to the VosFermes page
@@ -36,8 +73,13 @@ function Sidebar() {
               </Dropdown.Toggle>
 
                 <Dropdown.Menu className="bg-transparent border-0">
-                  <Dropdown.Item href="#action1" className="dropdown-item">Boumerdes 1</Dropdown.Item>
-                  <Dropdown.Item href="#action2" className="dropdown-item">Boumerdes 2</Dropdown.Item>
+                {farms.length > 0 ? (
+                  farms.map((farm) => (
+                  <Dropdown.Item key={farm.id} href="#action1" className="dropdown-item">{farm.name}</Dropdown.Item>
+                ))
+              ) : (
+                <p>{language === 'fr' ? 'Aucune ferme trouvée.' : 'لم يتم العثور على أي مزرعة.'}</p>
+              )}
                   <Dropdown.Item onClick={handleAjout} className="dropdown-item" >
                     <img
                       src={plusicone}
