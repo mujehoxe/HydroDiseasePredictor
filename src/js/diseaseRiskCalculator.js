84640c6547c900e984129685ae33c09322b5387a
SarahRayaneRoufiaBenzai <roufiabenzai@gmail.com>
2025-02-12 00:07:02 +0100

    xanthomonas formula

--- src/js/diseaseRiskCalculator.js
+++ src/js/diseaseRiskCalculator.js
@@ -45,10 +45,19 @@ const diseaseRiskCalculators = {
            return Math.round(Math.min(risk, 100));
     },
     xanthomonas: (temperature, humidity) => {
-      if (temperature < 20 || humidity < 50) return 0;
-      if (temperature >= 20 && temperature <= 30 && humidity >= 50 && humidity <= 70) return 40;
-      if (temperature > 30 && humidity > 70) return 70;
-      else return 90;
+      let risk = 0;
+      if (temperature < 25 || temperature >-2){
+        risk = -0.00399 * Math.pow(temperature, 4) 
+        + 0.2243 * Math.pow(temperature, 3) 
+        - 4.4602 * Math.pow(temperature, 2) 
+        + 39.50 * temperature 
+        - 109.09;
+      } else {risk = 0; }
+      
+      if (humidity >= 90){
+        risk += (53-(35/(humidity + 0.65)));
+      }
+           return Math.round(Math.min(risk, 100));
     },
     mildiou: (temperature, humidity) => {
       let risk = 0;
