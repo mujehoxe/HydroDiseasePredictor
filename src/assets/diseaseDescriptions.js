31d9bdaf0228b318c0e35391b777adbab0fcc9d4
SarahRayaneRoufiaBenzai <roufiabenzai@gmail.com>
2025-02-13 15:56:49 +0100

    more details in definitions

--- src/assets/diseaseDescriptions.js
+++ src/assets/diseaseDescriptions.js
@@ -2,39 +2,38 @@ const diseasesDescriptions = {
   pythium: {
     name: { fr: "Pythium", ar: "العفن المائي" },
     description: {
-      fr: "Le Pythium est une maladie fongique qui affecte principalement les jeunes plants, provoquant la fonte des semis. Elle se manifeste par des racines noircies et pourries, et un flétrissement des plants.",
-      ar:  "البيثيوم هو مرض فطري يصيب النباتات الصغيرة بشكل أساسي، مما يسبب تعفن الشتلات. يتميز بجذور سوداء ومتآكلة وذبول النباتات.",
+      fr: "Le Pythium est une maladie fongique qui affecte principalement les jeunes plants, provoquant la fonte des semis. Elle se manifeste par des racines noircies et pourries, et un flétrissement des plants. Les conditions favorables incluent une humidité excessive et des températures élevées. Pour prévenir cette maladie, il est crucial de maintenir une bonne aération et de contrôler l'humidité du sol.",
+      ar: "البيثيوم هو مرض فطري يصيب النباتات الصغيرة بشكل أساسي، مما يسبب تعفن الشتلات. يتميز بجذور سوداء ومتآكلة وذبول النباتات. تشمل الظروف المواتية ارتفاع الرطوبة ودرجات الحرارة العالية. للوقاية من هذا المرض، من الضروري الحفاظ على تهوية جيدة والتحكم في رطوبة التربة.",
     },
   },
   botrytis: {
     name: { fr: "Botrytis", ar: "البوتريتيس" },
     description: {
-      fr: "Le Botrytis est une maladie fongique qui se caractérise par l'apparition d'une moisissure grise sur les feuilles, les fleurs et les fruits. Elle est favorisée par des conditions humides et une mauvaise circulation de l'air.",
-      ar: "البوتريتيس هو مرض فطري يتميز بظهور عفن رمادي على الأوراق والأزهار والثمار. يزدهر في الظروف الرطبة وسوء تهوية الهواء.",
+      fr: "Le Botrytis est une maladie fongique qui se caractérise par l'apparition d'une moisissure grise sur les feuilles, les fleurs et les fruits. Elle est favorisée par des conditions humides et une mauvaise circulation de l'air. Cette maladie peut entraîner des pertes importantes de récolte si elle n'est pas contrôlée. Il est recommandé d'améliorer la ventilation et de réduire l'humidité pour limiter son développement.",
+      ar: "البوتريتيس هو مرض فطري يتميز بظهور عفن رمادي على الأوراق والأزهار والثمار. يزدهر في الظروف الرطبة وسوء تهوية الهواء. يمكن أن تؤدي هذه العدوى إلى خسائر كبيرة في المحاصيل إذا لم يتم التحكم فيها. يُنصح بتحسين التهوية وتقليل الرطوبة للحد من تطورها.",
     },
   },
   xanthomonas: {
     name: { fr: "Tache bactérienne des feuilles", ar: "التبقع البكتيري للأوراق" },
     description: {
-      fr: "La tache bactérienne des feuilles de la laitue est causée par la bactérie *Xanthomonas campestris pv. vitians*. Elle se manifeste par l'apparition de petites lésions angulaires imbibées d'eau, qui deviennent brunes à noires avec le temps. Ces taches sont souvent bordées d'un halo jaune et peuvent fusionner, entraînant un dessèchement du feuillage. La maladie est favorisée par une humidité élevée, des éclaboussures d'eau et des températures modérées. Elle se propage principalement par les semences infectées, l'eau d'irrigation contaminée et les éclaboussures de pluie.",
-      ar: "يُسبَّب التبقع البكتيري للأوراق في الخس بواسطة البكتيريا *Xanthomonas campestris pv. vitians*. يتميز بظهور آفات صغيرة مبللة بالماء ذات شكل زاوي، تتحول تدريجيًا إلى اللون البني أو الأسود. غالبًا ما تكون هذه البقع محاطة بهالة صفراء، وقد تتحد معًا مما يؤدي إلى جفاف الأوراق. تزدهر هذه العدوى في الظروف الرطبة، وتنتقل عبر البذور المصابة، ومياه الري الملوثة، ورذاذ المطر.",
+      fr: "La tache bactérienne des feuilles de la laitue est causée par la bactérie *Xanthomonas campestris pv. vitians*. Elle se manifeste par l'apparition de petites lésions angulaires imbibées d'eau, qui deviennent brunes à noires avec le temps. Ces taches sont souvent bordées d'un halo jaune et peuvent fusionner, entraînant un dessèchement du feuillage. La maladie est favorisée par une humidité élevée, des éclaboussures d'eau et des températures modérées. Elle se propage principalement par les semences infectées, l'eau d'irrigation contaminée et les éclaboussures de pluie. Pour la gestion, il est conseillé d'utiliser des semences saines et de pratiquer une rotation des cultures.",
+      ar: "يُسبَّب التبقع البكتيري للأوراق في الخس بواسطة البكتيريا *Xanthomonas campestris pv. vitians*. يتميز بظهور آفات صغيرة مبللة بالماء ذات شكل زاوي، تتحول تدريجيًا إلى اللون البني أو الأسود. غالبًا ما تكون هذه البقع محاطة بهالة صفراء، وقد تتحد معًا مما يؤدي إلى جفاف الأوراق. تزدهر هذه العدوى في الظروف الرطبة، وتنتقل عبر البذور المصابة، ومياه الري الملوثة، ورذاذ المطر. لإدارة هذا المرض، يُنصح باستخدام بذور صحية وممارسة تدوير المحاصيل.",
     },
-},
+  },
   mildiou: {
     name: { fr: "Mildiou", ar: " البياض الزغبي" },
     description: {
-      fr: "Le Mildiou est une maladie fongique causée par le pathogène 'Bremia lactucae'. Il se manifeste par des taches jaunes sur les feuilles, suivies d'une moisissure blanche au revers. Elle affecte également les fruits et les tiges.",
-      ar: "البياض الزغبي هو مرض فطري يظهر كبقع صفراء على الأوراق، يتبعها عفن أبيض على الجانب السفلي. يصيب الثمار والسيقان أيضًا.",
+      fr: "Le Mildiou est une maladie fongique causée par le pathogène 'Bremia lactucae'. Il se manifeste par des taches jaunes sur les feuilles, suivies d'une moisissure blanche au revers. Cette maladie peut affecter gravement la croissance des plantes et leur rendement. Il est essentiel de surveiller l'humidité et d'appliquer des fongicides appropriés pour contrôler son apparition.",
+      ar: "البياض الزغبي هو مرض فطري يظهر كبقع صفراء على الأوراق، يتبعها عفن أبيض على الجانب السفلي. يمكن أن تؤثر هذه العدوى بشكل كبير على نمو النباتات وإنتاجيتها. من الضروري مراقبة الرطوبة وتطبيق مبيدات فطرية مناسبة للسيطرة على ظهور هذا المرض.",
     },
   },
   oidium: {
     name: { fr: "Oïdium", ar: " البياض الدقيقي" },
     description: {
-      fr:  "L'Oïdium est une maladie fongique qui se manifeste par un feutrage blanc poudreux sur les feuilles, les tiges et les fruits. Elle réduit la photosynthèse et affaiblit les plantes.",
-      ar:  "البياض الدقيقي هو مرض فطري يظهر كطبقة بيضاء مسحوقية على الأوراق والسيقان والثمار. يقلل من التمثيل الضوئي ويضعف النباتات.",
+      fr: "L'Oïdium est une maladie fongique qui se manifeste par un feutrage blanc poudreux sur les feuilles, les tiges et les fruits. Elle réduit la photosynthèse et affaiblit les plantes, rendant celles-ci plus vulnérables à d'autres maladies. Pour prévenir l'oïdium, il est recommandé d'améliorer la circulation de l'air et d'éviter l'arrosage excessif.",
+      ar: "البياض الدقيقي هو مرض فطري يظهر كطبقة بيضاء مسحوقية على الأوراق والسيقان والثمار. يقلل من التمثيل الضوئي ويضعف النباتات، مما يجعلها أكثر عرضة لأمراض أخرى. للوقاية من البياض الدقيقي، يُنصح بتحسين تدفق الهواء وتجنب الري المفرط.",
     },
   },
 };
 
-export default diseasesDescriptions; 
-  
+export default diseasesDescriptions;
