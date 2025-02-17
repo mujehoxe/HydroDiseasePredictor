37b5c1bad406a61e41d6c32c4b85e9a77f1650ab
SarahRayaneRoufiaBenzai <roufiabenzai@gmail.com>
2025-02-18 00:04:06 +0100

    58 Algerian cities

--- src/Ajouter-une-ferme.jsx
+++ src/Ajouter-une-ferme.jsx
@@ -33,8 +33,16 @@ function Ajoutferme() {
   const [error, setError] = useState('');
 
   const algerianCities = [
-    'Algiers', 'Oran', 'Constantine', 'Annaba', 'Blida', 'Tlemcen',
-    'Setif', 'Batna', 'Bejaia', 'Tizi Ouzou', 'Djelfa',
+    'Adrar', 'Chlef', 'Laghouat', 'Oum El Bouaghi', 'Batna', 'Bejaia', 'Biskra',
+    'Bechar', 'Blida', 'Bouira', 'Tamanrasset', 'Tebessa', 'Tlemcen', 'Tiaret',
+    'Tizi Ouzou', 'Algiers', 'Djelfa', 'Jijel', 'Setif', 'Saida', 'Skikda',
+    'Sidi Bel Abbes', 'Annaba', 'Guelma', 'Constantine', 'Medea', 'Mostaganem',
+    'MSila', 'Mascara', 'Ouargla', 'Oran', 'El Bayadh', 'Illizi',
+    'Bordj Bou Arreridj', 'Boumerdes', 'El Tarf', 'Tindouf', 'Tissemsilt',
+    'El Oued', 'Khenchela', 'Souk Ahras', 'Tipaza', 'Mila', 'Ain Defla',
+    'Naama', 'Ain Temouchent', 'Ghardaia', 'Relizane', 'El MGhair',
+    'El Meniaa', 'Ouled Djellal', 'Bordj Baji Mokhtar', 'Beni Abbes',
+    'Timimoun', 'Touggourt', 'Djanet', 'In Salah', 'In Guezzam'
   ];
 
   const handleSubmit = async () => {
